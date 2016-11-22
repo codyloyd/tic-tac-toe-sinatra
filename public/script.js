db2b475bdd15796df7a384f37d8f89194975b773
Cody Loyd <codyloyd@gmail.com>
2016-11-22 05:44:46 -0600

    place X automatically

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   $(".square").click(function(){
     var value = $(this).attr("value")
+    $(this).html('X')
     $.post('/', {position: value, mark: 'X'},
       function (result) {
         location.reload()
